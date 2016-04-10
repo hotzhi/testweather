a03b5502d25a182b9117df5ae21af4d9db8bcf0e
hotzhi <hotzhi@gmail.com>
2016-04-10 21:48:55 +0800

    hide ChooseAreaActivity title

--- app/src/main/java/com/hotzhi/testweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/hotzhi/testweather/activity/ChooseAreaActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -53,8 +52,8 @@ public class ChooseAreaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
+        getSupportActionBar().hide();
 
         listView = (ListView) findViewById(R.id.list_view);
         titleText = (TextView) findViewById(R.id.title_text);
