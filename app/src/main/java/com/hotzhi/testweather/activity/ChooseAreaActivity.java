1ad4499c5578a112c3093650ef792ec09d03107d
hotzhi <hotzhi@gmail.com>
2016-04-17 09:15:51 +0800

    add the function which is showing the weather info

--- app/src/main/java/com/hotzhi/testweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/hotzhi/testweather/activity/ChooseAreaActivity.java
@@ -1,7 +1,10 @@
 package com.hotzhi.testweather.activity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -52,6 +55,14 @@ public class ChooseAreaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if(preferences.getBoolean("city_selected", false)) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
         setContentView(R.layout.choose_area);
         getSupportActionBar().hide();
 
@@ -66,9 +77,15 @@ public class ChooseAreaActivity extends AppCompatActivity {
                 if(currentLevel == LEVEL_PROVINCE) {
                     selectedProvince = provinceList.get(i);
                     queryCities();
-                } else {
+                } else if(currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(i);
                     queryCounties();
+                } else if(currentLevel == LEVEL_COUNTY) {
+                    String countyCode = countyList.get(i).getCountyCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+                    intent.putExtra("county_code", countyCode);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
